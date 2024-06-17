fe4143bdc20cb9df3d6710e174161972edc50df2
Jáderson Válner Ferreira de Lima <160531175+jaderson13@users.noreply.github.com>
2024-06-17 15:50:51 -0300

    Add files via upload

---
+++ trabalho ludo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_ludo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor[] cores = new ConsoleColor[5];
+            cores[0] = ConsoleColor.Green;
+            Console.BackgroundColor = cores[0];
+            Console.WriteLine("");
+        }
+    }
+}
