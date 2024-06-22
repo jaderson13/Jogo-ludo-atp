6fedda2676310e8085b787f8893f7b49a410b1a4
Jáderson Válner Ferreira de Lima <160531175+jaderson13@users.noreply.github.com>
2024-06-21 22:32:44 -0300

    Add files via upload

---
+++ trabalho ludo/Jogador.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_ludo
+{
+    internal class Jogador
+    {
+        public Peao[] vet_peoes = new Peao[4];
+        public ConsoleColor cor;
+        public int[] jogadas = new int[15];
+        
+        public Jogador(ConsoleColor cor)
+        {
+            this.cor = cor;
+        }
+
+        public void Criar_peoes()
+        {
+            for(int i = 0; i < this.vet_peoes.Length; i++)
+            {
+                Peao peao = new Peao(this.cor,i,0);
+                this.vet_peoes[i] = peao;
+            }
+        }
+
+        public bool Jogar_dado(ref bool jogada_extra)
+        {
+            int numero = 6, cont6 = 0;
+            bool passar_vez = false;
+            Random dado = new Random();
+
+            if (jogada_extra)
+            {
+              
+                for (int i = 0; numero==6 && i < this.jogadas.Length; i++)
+                {
+                    if (this.jogadas[i] == 0)
+                    {
+                        numero = dado.Next(1, 7);
+                        Console.WriteLine($"\nVocê tirou {numero}");
+                        this.jogadas[i] = numero;
+                        break;
+                    }
+                }
+            }
+
+            else
+            {
+                for (int i = 0; numero == 6 && i < jogadas.Length-2; i++)
+                {              
+                    numero = dado.Next(1, 7);
+
+                    this.jogadas[i] = numero;
+                    Console.WriteLine($"\nVocê tirou {numero}");
+
+                    if (numero == 6)
+                    {
+                        cont6++;
+                    }
+                }
+
+                if (cont6 == 3)
+                {
+                    passar_vez = true;
+                    Console.WriteLine("\nPerdeu a vez!");
+
+                    for (int i = 0; i < this.jogadas.Length; i++)
+                    {
+                        this.jogadas[i] = 0;
+                    }
+                }
+            }
+
+            return passar_vez;
+        }
+
+        public void Mover_peao(int peao_escolhido, int num_dado, out bool checkpoint, out bool peao_vitoria,bool sair_da_casa)
+        {
+
+            int coluna_peao;
+            int[] vet_checkpoints = { 1, 9, 14, 22, 27, 35, 40, 48, 52, 53, 54, 55, 56, 57 };                                          
+            peao_vitoria = checkpoint = false;
+            peao_escolhido -= 1; 
+
+            if (sair_da_casa)
+            {
+                for (int i = 0; i < this.vet_peoes.Length; i++)
+                {
+                    if (this.vet_peoes[i].coluna == 0)
+                    {
+                        if (peao_escolhido == this.vet_peoes[i].numero)
+                        {
+                            this.vet_peoes[i].coluna = this.vet_peoes[i].coluna++;
+                            checkpoint = true;
+                        }
+                    }
+                }
+            }
+
+            //jogada realizada se o peão em questão estiver fora da casa 0 ou se pelo menos 1 peão saiu da casa 0
+            else if (!sair_da_casa)
+            {
+                for (int i = 0; i < this.vet_peoes.Length; i++)
+                {
+                    if (this.vet_peoes[i].numero == peao_escolhido)
+                    {
+
+                        this.vet_peoes[i].coluna = this.vet_peoes[i].coluna + num_dado;
+                        coluna_peao = this.vet_peoes[i].coluna + num_dado;
+
+                        for (int j = 0; j < vet_checkpoints.Length; j++)
+                        {
+                            if (coluna_peao == vet_checkpoints[i])
+                            {
+                                checkpoint = true;
+                                break;
+                            }
+                        }
+
+                        if (coluna_peao == 57)
+                        {
+                            peao_vitoria = true;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public bool Validar_turno()// testar se pelo meno existe uma jogada válida no turno seguinte
+        {
+  
+            for(int i = 0; i < this.jogadas.Length; i++)
+            {
+                if (this.jogadas[i] == 0)
+                {
+                    break;
+                }
+
+                for(int j = 0; j < this.vet_peoes.Length; j++)
+                {
+                    if (this.vet_peoes[i].coluna == 0)
+                    {
+                        if (this.jogadas[i] == 6)
+                        {
+                            return true;
+                        }
+                    }
+
+                    else if (this.vet_peoes[j].coluna + this.jogadas[i] <= 57)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+
+            return false;
+
+        }
+
+        public bool Validar_jogada(int jogada_escolhida,int peao_escolhido_coluna,out bool sair_da_casa)
+        {
+           sair_da_casa =  false;
+           bool jogada_invalida = true;
+
+            if (peao_escolhido_coluna == 0)
+            {
+                if (jogada_escolhida == 6)
+                {
+                    sair_da_casa = true;
+                    jogada_invalida = false;
+                }
+            }
+
+            else if (peao_escolhido_coluna + jogada_escolhida <= 57)
+            {
+               jogada_invalida =  false;
+            }
+
+            if (!jogada_invalida)
+            {
+                for (int j = 0; j < this.jogadas.Length; j++)
+                {
+                    if (this.jogadas[j] == jogada_escolhida)
+                    {
+                        this.jogadas[j] = 0;
+                        break;
+                    }
+                }
+
+            }
+
+            return jogada_invalida;
+
+        }
+
+        public int Contar_jogadas_restantes()
+        {
+            int jogadas_restantes = 0;
+
+            for (int j = 0; j < this.jogadas.Length; j++)
+            {
+                if (this.jogadas[j] != 0)
+                {
+                    jogadas_restantes++;
+                }
+            }
+
+            return jogadas_restantes;
+        }
+
+        public void Verificar_jogadas_disponiveis(int peao_coluna,int[] jogadas) //Mostrar ao jogador as jogadas disponíveis para determinado peão
+        {
+            for (int i = 0; i < this.jogadas.Length; i++)
+            {
+                if (this.jogadas[i] == 0)
+                {
+                    break;
+                }
+
+                if (peao_coluna == 0)
+                {
+                    if (this.jogadas[i] == 6)
+                    {
+                        jogadas[i] = this.jogadas[i];
+                    }
+                }
+
+                else if (peao_coluna + this.jogadas[i] <= 57)
+                {
+                    jogadas[i] = this.jogadas[i];
+                }
+
+            }
+        }
+
+        public void Verificar_peoes_disponiveis(int[] peoes)
+        {
+            for (int i = 0; i < this.vet_peoes.Length; i++)
+            {
+                for (int j = 0; j < this.jogadas.Length; j++)
+                {
+                    if (this.vet_peoes[i].coluna == 0)
+                    {
+                        if (this.jogadas[j] == 6)
+                        {
+                            peoes[i] = this.vet_peoes[i].numero;
+                            break;
+                        }
+                    }
+
+                    else if (this.vet_peoes[i].coluna + this.jogadas[j] <= 57)
+                    {
+                        peoes[i] = this.vet_peoes[i].numero;
+                        break;
+                    }
+                }
+
+            }
+       
+        }
+
+    }
+}
---
+++ trabalho ludo/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace trabalho_ludo
+{
+    internal class Program
+    {
+        static int Ludo_atp()
+        {
+            int jogador_vencedor, jogadas_restantes = 0, jogador_capturado, jogada_escolhida;
+
+            int peao_capturado, n_jogadores = 0,peao_escolhido;
+
+            int[] peoes_disponiveis = new int[4];
+            int[] jogadas_disponiveis = new int[15];
+
+            bool jogada_invalida, checkpoint, peao_vitoria;
+            bool captura, passar_vez, jogada_extra = false;
+            bool sair_da_casa;
+
+            Console.WriteLine("Digite o número de jogadores (2) ou (4): ");
+            n_jogadores = int.Parse(Console.ReadLine());
+
+            Jogo ludo = new Jogo(n_jogadores,ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Red, ConsoleColor.Blue);
+
+            do
+            {
+                for (int i = 0; i < ludo.vet_jogadores.Length; i++)
+                {
+                    passar_vez = ludo.vet_jogadores[i].Jogar_dado(ref jogada_extra); //passar a vez apenas se tirar 3 6 seguidos
+
+                    if (!passar_vez) //testar se existe realmente uma jogada disponível
+                    {
+                        passar_vez = ludo.vet_jogadores[i].Validar_turno();
+                    }
+
+                    if (!passar_vez)
+                    {
+                        do
+                        {
+                            do
+                            {
+                                jogada_extra = false;
+
+                                Console.WriteLine($"Qual peão deseja mover?\nPeões disponíveis no momento: ");
+                                ludo.vet_jogadores[i].Verificar_peoes_disponiveis(peoes_disponiveis);
+                                ludo.Exibir_informacoes(peoes_disponiveis);
+                                peao_escolhido = int.Parse(Console.ReadLine());
+
+                                Console.WriteLine($"\nQual jogada deseja gastar?\nJogadas disponíveis com o peão escolhido: ");
+                                ludo.vet_jogadores[i].Verificar_jogadas_disponiveis(peao_escolhido,jogadas_disponiveis);
+                                ludo.Exibir_informacoes(jogadas_disponiveis);
+                                jogada_escolhida = int.Parse(Console.ReadLine());
+
+                                jogada_invalida = ludo.vet_jogadores[i].Validar_jogada(jogada_escolhida, ludo.vet_jogadores[i].vet_peoes[peao_escolhido-1].coluna, out sair_da_casa);
+                                //validar jogada antes de mover
+
+                                if (jogada_invalida)
+                                {
+                                    Console.WriteLine("\nJogada inválida,tente novamente!");
+                                }
+
+                            } while (jogada_invalida);
+
+                            ludo.vet_jogadores[i].Mover_peao(peao_escolhido, jogada_escolhida, out checkpoint, out peao_vitoria, sair_da_casa);
+                            //E se o peão que for movido chegar no destino, o jogador pode jogar o dado mais uma vez
+
+                            if (!checkpoint)
+                            {//trabalhar jogada extra
+                                captura = ludo.Capturar_peao(ludo.vet_jogadores[i].vet_peoes[peao_escolhido].coluna, i, out jogador_capturado, out peao_capturado);
+
+                                if (captura)
+                                { //jogar novamente e testar tudo dnv
+                                    jogada_extra = true;
+                                    Console.WriteLine($"Você capturou o peão {peao_capturado + 1} do jogador {jogador_capturado + 1}");
+                                    ludo.vet_jogadores[i].Jogar_dado(ref jogada_extra);
+                                    ludo.Retornar_casa(jogador_capturado, peao_capturado);
+                                }
+                            }
+
+
+                        } while (jogadas_restantes == 0);
+                    }
+                    
+                }
+            } while (!ludo.Vitoria(out jogador_vencedor));
+
+            return jogador_vencedor;
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
