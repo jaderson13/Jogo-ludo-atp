871376f7e5300af63268953f130e0ca9116661e0
Jáderson Válner Ferreira de Lima <160531175+jaderson13@users.noreply.github.com>
2024-06-22 19:30:44 -0300

    Add files via upload

---
+++ trabalho ludo/Peao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_ludo
+{
+    internal class Peao
+    {
+        public ConsoleColor cor;
+        public int numero;
+        public int coluna;
+        //atributo de validação
+        public Peao(ConsoleColor cor,int numero,int coluna)
+        {
+            this.cor = cor;
+            this.numero = numero;
+            this.coluna = coluna;
+        }
+    }
+}
