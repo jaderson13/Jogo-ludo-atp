7208c22fb7adeef16b659140c5dec09c6428bacd
Jáderson Válner Ferreira de Lima <160531175+jaderson13@users.noreply.github.com>
2024-06-26 22:20:52 -0300

    Add files via upload

---
+++ trabalho ludo/Tabuleiro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using trabalho_ludo;
+
+namespace trabalho_ludo
+{
+    internal class Tabuleiro
+    {
+        public string[,] tabuleiro;
+        public string log_partida;
+        public StreamWriter log_escrito = new StreamWriter("log.txt", false, Encoding.UTF8);
+        public StreamReader log_leitura = new StreamReader("D:\\Usuário\\Documentos\\- Algoritmos C#\\- Trabalho ludo\\trabalho ludo\\bin\\Debug\\log.txt", Encoding.UTF8);
+        
+    }
+}
